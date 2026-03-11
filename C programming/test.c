ef9b72d463a13b2e0eea614a022a4c22160bc5ec
SiRyus1111 <anniefun1111@gmail.com>
2026-03-11 22:32:57 +0900

    First commit. add README.md / .gitignore / tasks.json / test.c

---
+++ C programming/test.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void){
+    printf("결과값 : %d\n", 3 + 2);
+    printf("결과값 : %d\n", 3 - 2);
+    printf("결과값 : %d\n", 3 * 9);
+
+    printf("10진수를 8진수로 변환 : %o\n", 3 * 9);
+
+    return 0;
+}
